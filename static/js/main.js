091846ef828f85d378b6992921535c46da868679
GhostWheat <32331476+GhostWheat@users.noreply.github.com>
2023-05-17 16:51:31 -0500

    logic is sound; working on puzzle layout

--- static/js/main.js
+++ static/js/main.js
@@ -1,129 +1,98 @@
 let numOfRows = 10;
 let numOfCols = 10;
 
-// var lastClicked;
-// var grid = clickableGrid(numOfRows, numOfCols, function (el, row, col, i) {
-    // console.log("You clicked on element:",el);
-    // console.log("You clicked on row:",row);
-    // console.log("You clicked on col:",col);
-    // console.log("You clicked on item #:",i);
+let rowHeadersWidth = (numOfCols * 7).toString() + 'px';
+let colHeadersHeight = (numOfrows * 7).toString() + 'px';
 
 
-// });
-
-// class niceGrid {
-//     constructor(rows, cols) {
-//         this.rows = rows;
-//         this.cols = cols;
-//     }
- 
-//     var grid = document.createElement('table');
-//     grid.className = 'grid';
-//     for (var r = 0; r < rows; ++r) {
-//         var tr = grid.appendChild(document.createElement('tr'));
-//         for (var c = 0; c < cols; ++c) {
-//             var cell = tr.appendChild(document.createElement('td'));
-//             cell.className = 'off';
-//             cell.innerHTML = 0;
-//             cell.id = 'c' + c + 'r' + r;
-
-//             cell.addEventListener('click', clickACell(cell))
-
-
-
-
-// }
-
-// function clickableGrid(rows, cols, callback) {
 
 function createNiceGrid(rows, cols) {
     var grid = document.createElement('table');
     grid.className = 'grid';
     for (var r = 0; r < rows; ++r) {
         var tr = grid.appendChild(document.createElement('tr'));
+        tr.className = 'tr';
         for (var c = 0; c < cols; ++c) {
             var cell = tr.appendChild(document.createElement('td'));
             cell.className = 'off';
-            // cell.innerHTML = 0;
+            cell.innerHTML = 0;
             cell.id = 'c' + c + 'r' + r;
-
-            // cell.onclick = cellToggle(cell);
-
-            // cell.addEventListener('click', (function (el, r, c, i) {
-            //     return function () {
-            //         callback(el, r, c, i);
-            //     }
-            // })(cell, r, c, i), false);
+            let permaID = ''
+            permaID = cell.id
+            // cell.addEventListener('click', cellToggle(cell))
+            // cell.onclick = () => { cellToggle(permaID) }
 
         }
     }
     grid.addEventListener('click', (event) => {
-       event.target.className = 'off' ? 'on' : 'off';
+        let h = event.target.innerHTML;
+        let c = event.target.className;
+
+        if (c == 'on' || c == 'off') {
+
+            // event.target.innerHTML = 1 ? ((event.target.className = 'off'), event.target.className = 0)
+            //     : (event.target.className = 'on', event.target.className = 1)
+
+            if (event.target.innerHTML == 1) {
+                event.target.innerHTML = 0;
+                event.target.className = 'off'
+            } else {
+                event.target.innerHTML = 1;
+                event.target.className = 'on'
+            }
+            
+        }
+
     })
-    
-    // if (event.target.innerHTML == 1) {
-    //     event.target.innerHTML = 0;
-    //     event.target.className = 'off'
-    // } else {
-    //     event.target.innerHTML = 1;
-    //     event.target.className = 'on'
-    // }
-    // return)
+
+
+
     return grid;
 }
 
-// function cellToggle(event) {
-//     // console.log(cel.innerHTML)
-//     if (event.target.innerHTML == 1) {
-//         event.target.innerHTML = 0;
-//         event.target.className = 'off'
-//     } else {
-//         event.target.innerHTML = 1;
-//         event.target.className = 'on'
-//     }
-//     return
-// }
 
-function createRowHeaders(rows) {
+function createRowHeadersTable(rows) {
     var rowHeadersTable = document.createElement('table');
-    rowHeadersTable.className = 'rowHeaders';
+    rowHeadersTable.id = 'rowHeaders';
 
     for (let r = 0; r < rows; r++) {
         var rowHeader = rowHeadersTable.appendChild(document.createElement('tr'));
         var cell = rowHeader.appendChild(document.createElement('td'));
-        cell.innerHTML = 'Hi'
+        cell.innerHTML = '1&ensp;2&ensp;3&ensp;4&ensp;11&ensp;21&ensp;1'
         cell.id = 'rh' + r
     }
     return rowHeadersTable;
 }
 
-function createColHeaders(cols) {
+function createColHeadersTable(cols) {
     var colHeadersTable = document.createElement('table');
-    colHeadersTable.className = 'colHeaders';
+    colHeadersTable.id = 'colHeaders';
 
     var colHeader = colHeadersTable.appendChild(document.createElement('tr'));
     for (let c = 0; c < cols; c++) {
         var cell = colHeader.appendChild(document.createElement('td'));
-        cell.innerHTML = 'Hi'
+        cell.innerHTML = '1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>'
         cell.id = 'ch' + c
     }
     return colHeadersTable;
 }
 
+function createUpperLeftSpacer()
+
 grid = createNiceGrid(numOfRows, numOfCols)
-rowClues = createRowHeaders(numOfRows)
-colClues = createColHeaders(numOfCols);
+rowClues = createRowHeadersTable(numOfRows)
+colClues = createColHeadersTable(numOfCols);
 
 document.getElementById('puzzle_box').appendChild(grid);
 document.getElementById('puzzle_box').appendChild(rowClues);
 document.getElementById('puzzle_box').appendChild(colClues);
 
-    function calcHeaders() {
+function calcHeaders() {
     let rowString = ''
     let colString = ''
     for (let r = 0; r < numOfRows; r++) {
-        console.log('starting on row:', r)
-        rowString = hashVector(r, numOfRows, '  ');
+        // console.log('starting on row:', r)
+        rowString = hashVector(r, numOfRows, '&ensp;');
         document.getElementById('rh' + r).innerHTML = rowString;
     }
 
@@ -134,54 +103,6 @@ document.getElementById('puzzle_box').appendChild(colClues);
     }
 }
 
-// function hashRow(row) {
-//     let ongoingString = '';
-//     let adder = 0;
-//     for (let i = 0; i < numOfCols; i++) {
-//         let slice = [0,0,0]
-//         let text = ''
-//         if (i > 0) {
-//             slice[0] = document.getElementById('c' + (i - 1) + 'r' + row).innerHTML
-//         }
-
-//         slice[1] = document.getElementById('c' + i + 'r' + row).innerHTML
-
-//         if (i < (numOfRows - 1)) {
-//             slice[2] = document.getElementById('c' + (i + 1) + 'r' + row).innerHTML
-//         } else {
-//             slice[2] = -1
-//         };
-
-//         text = slice.toString();
-//         console.log(text)
-//         // logic for checking contiguous blocks of ON cell
-//         switch (text) {
-
-//             case ('0,1,1'):
-//                 adder += 1;
-//                 break;
-
-//             case ('1,1,0' || '0,1,0'):
-//                 adder += 1;
-//                 ongoingString += adder.toString() + ' ';
-//                 adder = 0;
-//                 break;
-
-//             case ('1,1,-1' || '0,1,-1'):
-//                 adder += 1;
-//                 ongoingString += adder.toString();
-//                 adder = 0;
-//                 break;
-
-//             // default:
-//                 // console.log(adder)
-//                 // console.log(ongoingString)
-//                 // break;
-
-//         }
-//     }
-//     return ongoingString
-// }
 
 function hashVector(vec, vectorLength, breaker) {
     let ongoingString = '';
@@ -194,7 +115,7 @@ function hashVector(vec, vectorLength, breaker) {
         let nextCoord = '';
 
         switch (breaker) {
-            case '  '://this means we're going across a row
+            case '&ensp;'://this means we're going across a row
                 thisCoord = 'c' + i.toString() + 'r' + vec.toString();
                 nextCoord = 'c' + (i + 1).toString() + 'r' + vec.toString();
                 break;
@@ -217,7 +138,6 @@ function hashVector(vec, vectorLength, breaker) {
         // logic for checking contiguous blocks of ON cell
 
         console.log('this:', thisCoord, 'next:', nextCoord, ';', text)
-
 
         switch (text) {
 
